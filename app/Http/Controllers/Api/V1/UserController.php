f111c07725c86c5f2bae65ca4f41ff2865ae8fc4
FraisVI <FraisVI@yandex.ru>
2024-10-29 03:35:57 +0700

    Correct data and response code

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
 use App\Providers\UserService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
